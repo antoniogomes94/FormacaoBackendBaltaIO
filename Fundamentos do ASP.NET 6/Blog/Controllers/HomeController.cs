e7871c76ccd7781aa62d64c8fc894ec6676d671f
Antonio Gomes <antoniogomes313@outlook.com>
2022-08-11 08:20:20 -0300

    Crud Category

---
+++ Fundamentos do ASP.NET 6/Blog/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("")]
+        //Endpoint para helath check para ver se a api está ok
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
